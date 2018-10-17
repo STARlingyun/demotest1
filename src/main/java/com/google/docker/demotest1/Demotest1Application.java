85f17974aa506a1b395c73aff76e8ba089b0f335
star <m18510078938@163.com>
2018-10-17 18:52:55 +0800

    Initial commit

---
+++ src/main/java/com/google/docker/demotest1/Demotest1Application.java
@@ -0,0 +1,12 @@
+package com.google.docker.demotest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demotest1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demotest1Application.class, args);
+    }
+}
---
+++ src/main/java/com/google/docker/demotest1/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.google.docker.demotest1.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
